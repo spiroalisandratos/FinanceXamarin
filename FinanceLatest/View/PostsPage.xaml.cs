8c523129f12da2d6239e78db7f9711d74394a163
Spiro Alisandratos <spiroalisandratos@hotmail.com>
2020-06-09 21:58:15 +1000

    New finance app working

---
+++ FinanceLatest/View/PostsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using FinanceLatest.Models;
+using Xamarin.Forms;
+
+namespace FinanceLatest.View
+{
+    public partial class PostsPage : ContentPage
+    {
+        public PostsPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        public PostsPage(Item item)
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            webView.Source = item.ItemLink;
+        }
+    }
+}
